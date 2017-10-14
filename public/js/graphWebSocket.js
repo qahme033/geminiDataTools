f005585f51aaa3ba5fb551eab1937961c21ed01e
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-14 19:20:05 -0400

    save

--- public/js/graphWebSocket.js
+++ public/js/graphWebSocket.js
@@ -7,14 +7,21 @@ var tradesBuffer = [];
 
 
 function main(){
-	var socket = new WebSocket("wss://api.gemini.com/v1/marketdata/" + symbol);
+	var graph = new Graph();
+	graph.draw();
+	var socket = new WebSocket("wss://api.gemini.com/v1/marketdata/" + symbol + "?heartbeat=true");
 	socket.onmessage = handleMessage;
 	//socket.run_forever(ping_interval=10000)
 }
 
 function handleMessage(event) {
 	var data = JSON.parse(event.data)
-	if(data.events[0].type == "trade"){
-  		tradesBuffer.push(data)
+	console.log(data.events[0].type)
+  	if(data.type == "heartbeat"){
+  		console.log("heartbeat")
+  	} else if(data.events[0].type == "trade"){
+  		tradesBuffer.push({timestampms : data.timestampms, price : data.price})
+  		console.log(tradesBuffer)
+
   	}
 }
