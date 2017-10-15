3e021ea75d1cfdbc50a64688ce9e3bef12b79b8c
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-14 23:03:50 -0400

    getting first 500 rows and storing data as jsonb

--- app/controllers/fetch.js
+++ app/controllers/fetch.js
@@ -1,29 +1,40 @@
 var express = require('express'),
-  router = express.Router();
+  router = express.Router(),
+  request = require('request');
 
 var Sequelize = require('sequelize')
 const connection = new Sequelize('trades', 'qasimahmed', 'pass', {
 dialect: 'postgres'});
-
-var TradesTable = connection.define('TradeTable', {
+var TradesTable = connection.define('TradesTable', {
+	tid: { type: Sequelize.INTEGER, primaryKey: true },
 	data : Sequelize.JSONB,
 })
-
 connection.sync().then(function(trade){
-    	console.log(trade)
-    })
+    	//console.log(trade)
+ })
 
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/fetch', function (req, res, next) {
-    res.render('fetch', {
-      title: 'Fetching Page',
-     // articles: articles
-    });
-    // TradeTable.findById(1).then(function(trade){
-    // 	console.log(trade)
-    // })
+router.get('/fetch',function (req, res, next) {
+    // res.render('fetch', {
+    //   title: 'Fetching Page',
+    //  // articles: articles
+    // });
+     var symbol = req.query.symbol;
+     console.log(symbol)
+     request('https://api.gemini.com/v1/trades/'+symbol+ '?limit_trades=1000&since=1444311607801', function(error, response, body){
+        if(!error){
+          res.send(body)
+        }
+        var trades = JSON.parse(body)
+
+        for(trade in trades) //changing structure to tid with data 
+        	trades[trade] = {tid : trades[trade].tid, data : trades[trade]}
+        console.log(trades)
+
+        TradesTable.bulkCreate(trades)
+      })
 });
