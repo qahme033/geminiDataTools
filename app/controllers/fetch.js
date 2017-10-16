f469ac6cc6dee2dc3c4563d676effc7e379d8bd7
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-16 09:33:16 -0400

    reflecting schema change

--- app/controllers/fetch.js
+++ app/controllers/fetch.js
@@ -16,15 +16,18 @@ module.exports = function (app) {
 
 router.get('/fetch',function (req, res, next) {
 	 var symbol = req.query.symbol;
+
 	TradesTable = connection.define('TradesTable_'+ symbol, {
-		tid: { type: Sequelize.INTEGER, primaryKey: true },
+		tid: { type	: Sequelize.INTEGER, primaryKey: true },
 		timestampms : Sequelize.BIGINT,
-		price : Sequelize.DOUBLE,
-		amount: Sequelize.DOUBLE,
-		type : Sequelize.STRING,
+		price 		: Sequelize.DOUBLE,
+		amount		: Sequelize.DOUBLE,
+		type 		: Sequelize.STRING,
+		day 		: Sequelize.INTEGER,
+		month		: Sequelize.INTEGER,
+		year 		: Sequelize.INTEGER,
 	})
 	synchPromise = connection.sync();
-     console.log(symbol)
      var currentTime = new Date()
      getNewBatches(symbol,res);
 
@@ -33,16 +36,17 @@ router.get('/fetch',function (req, res, next) {
 
 function getNewBatches(symbol,res, lastTime){
 	var startTime = new Date().getTime();
+    console.log(lastTime)
     if(!lastTime) {
     	synchPromise = synchPromise.then(function(){
 	 		return TradesTable.max('timestampms').then(max => {
-				console.log("Last time was " + max)
+				console.log("Last time was " + max + "  " + new Date(max).toString())
 			 	return max
 			})
 	 	})
     }
 	synchPromise.then(function(lastTime){
-		return request('https://api.gemini.com/v1/trades/'+symbol+ '?limit_trades=1000&since=' + (lastTime+1), function(error, response, body){
+		return request('https://api.gemini.com/v1/trades/'+symbol+ '?limit_trades=1000&since=' + (lastTime+1) , function(error, response, body){
 	       	var trades = JSON.parse(body)
 		    TradesTable.bulkCreate(trades).then(function(){
 		    	var endTime = new Date().getTime();
@@ -52,6 +56,9 @@ function getNewBatches(symbol,res, lastTime){
 		    		setTimeout(function(){getNewBatch(symbol,res)}, 250 - timeElapsed)
 		    	else
 		    		getNewBatches(symbol, res)
+		    })
+		    .catch(function(e){
+		    	console.log(e)
 		    })
 	    })
 	})
