3fea4308227c9553159eef6b0b812ecf12fd4bcd
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-19 07:27:21 -0400

    saving more redundant data to help queries

--- app/controllers/fetch.js
+++ app/controllers/fetch.js
@@ -26,6 +26,9 @@ router.get('/fetch',function (req, res, next) {
 		day 		: Sequelize.INTEGER,
 		month		: Sequelize.INTEGER,
 		year 		: Sequelize.INTEGER,
+		hour 		: Sequelize.INTEGER,
+		minute		: Sequelize.INTEGER,
+		second 		: Sequelize.INTEGER,
 	})
 	synchPromise = connection.sync();
      var currentTime = new Date()
@@ -48,6 +51,7 @@ function getNewBatches(symbol,res, lastTime){
 	synchPromise.then(function(lastTime){
 		return request('https://api.gemini.com/v1/trades/'+symbol+ '?limit_trades=1000&since=' + (lastTime+1) , function(error, response, body){
 	       	var trades = JSON.parse(body)
+	       	insertDateRedund(trades)
 		    TradesTable.bulkCreate(trades).then(function(){
 		    	var endTime = new Date().getTime();
 		    	var timeElapsed = endTime - startTime
@@ -62,4 +66,16 @@ function getNewBatches(symbol,res, lastTime){
 		    })
 	    })
 	})
+}
+
+function insertDateRedund(trades){
+   	for(row in trades){
+		var date = new Date(trades[row].timestampms)
+		trades[row]["day"]= date.getDate()
+		trades[row]["month"]= (date.getMonth() + 1) //js month starts at 0
+		trades[row]["year"]= date.getFullYear()
+		trades[row]["hour"]= date.getHours()
+		trades[row]["minute"]= date.getMinutes()
+		trades[row]["second"]= date.getSeconds()
+	}
 }
